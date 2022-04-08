f1d957aa24ceb720fedd89edb6c9ca92f4b097a1
Ashraf Ali <ashrafalicitp@gmail.com>
2022-04-09 00:03:24 +0600

    firebase private router file init

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAq51ZDzzmhJ2QwZPaE_faQb9SgqtOiRbg",
+  authDomain: "fir-private-route-480ee.firebaseapp.com",
+  projectId: "fir-private-route-480ee",
+  storageBucket: "fir-private-route-480ee.appspot.com",
+  messagingSenderId: "389929142578",
+  appId: "1:389929142578:web:a2f14cccc22e5ced7b6f2b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
